217a1b6c3f578eff56daafb074014641b127ae9c
manoy96 <mfewcar@gmail.com>
2019-08-13 17:20:38 -0600

    added a catch to show errors

--- src/components/spells.vue
+++ src/components/spells.vue
@@ -1,29 +1,31 @@
 <template>
-    <div class="spells">
-        <h2>This is the spells component</h2>
-        <div>{{}}</div>
-    </div>
-    
+  <div class="spells">
+    <h2>This is the spells component</h2>
+    <div>{{}}</div>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 
 export default {
-    name: "spells",
-    mounted() {
-        const axios = require("axios");
+  name: "spells",
+  mounted() {
+    const axios = require("axios");
 
-        axios.get("https://cors-anywhere.herokuapp.com/http://dnd5eapi.co/api/")
-        .then(function(response) {
-            console.log(response);
-            
-        })
-    }   
-}
+    axios
+      .get("https://cors-anywhere.herokuapp.com/http://dnd5eapi.co/api/")
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
+      });
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
 
