5baf16efd0c04af8159e2bc255064b379add2a41
manoy96 <mfewcar@gmail.com>
2019-08-13 20:00:50 -0600

    took off the bind on classes.

--- src/components/spells.vue
+++ src/components/spells.vue
@@ -2,7 +2,7 @@
   <div class="spells">
     <h2>This is the spells component</h2>
     <ul>
-      <li :="classes in classes">{{classes}}</li>
+      <li>{{ classes }}</li>
     </ul>
   </div>
 </template>
@@ -14,21 +14,9 @@ export default {
   name: "spells",
   data() {
     return {
-      classes: null,
+      classes: null
     };
   },
-//   methods: {
-//     getClasses: function() {
-//       axios
-//         .get(
-//           "https://cors-anywhere.herokuapp.com/http://dnd5eapi.co/api/classes"
-//         )
-//         .then(response => {
-//           this.classes = response;
-//         })
-//         .catch(error => console.log(error));
-//     }
-//   },
   mounted() {
     const axios = require("axios");
     const url =
@@ -36,8 +24,10 @@ export default {
 
     axios
       .get(url)
-    //   .then(response => console.log(response.data.results))
-      .then(res => {this.classes = res.data.results})
+      //   .then(response => console.log(response.data.results))
+      .then(res => {
+        this.classes = res.data.results;
+      })
       .catch(error => console.log(error));
   }
 };
