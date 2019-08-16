32a1a3686e385dac07ada008adcc61d74d36f3e0
manoy96 <mfewcar@gmail.com>
2019-08-15 19:33:16 -0600

    added button with info, and rendered multiple cards

--- src/components/spells.vue
+++ src/components/spells.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="spells">
-    <h2>This is the spells component</h2>
-    <h2 v-for="(classes) in classes">{{ classes.name }}</h2>
-  </div>
+    <!-- <h2 v-for="(test) in classes">{{ test.name }}</h2> -->
+    <v-card max-width="344" class="ml-3" v-for="(test) in classes">
+      <v-card-title>{{ test.name }}</v-card-title>
+      <v-btn :click="getSpells('test')">get info</v-btn>
+    </v-card>
+  </div> 
 </template>
 
 <script>
@@ -10,22 +13,36 @@ import axios from "axios";
 
 export default {
   name: "spells",
+  methods: {
+      getSpells: (myUrl) => {
+        console.log(myUrl)
+    },
+  },//end methods
   data() {
     return {
       classes: null
     };
-  },
+  },//end data
   mounted() {
     const axios = require("axios");
-    const url = "https://cors-anywhere.herokuapp.com/http://dnd5eapi.co/api/classes";
+    const url =
+      "https://cors-anywhere.herokuapp.com/http://dnd5eapi.co/api/spells";
 
-    axios.get(url)
-    //   .then(response => console.log(response.data.results))
+    axios
+      .get(url)
+      //   .then(response => console.log(response.data.results))
       .then(res => {
-        this.classes = res.data.results
+        this.classes = res.data.results;
       })
       .catch(error => console.log(error));
-  }
+
+
+
+
+
+
+
+  },//end mounted
 };
 </script>
 
