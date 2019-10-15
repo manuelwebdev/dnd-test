17eaeb11935f584a3b11ecb8c7a538dab88f6a76
manoy96 <mfewcar@gmail.com>
2019-10-14 23:01:39 -0600

    creating a card to display all spells

--- src/components/spells.vue
+++ src/components/spells.vue
@@ -3,9 +3,20 @@
     <!-- <v-card max-width="344" class="ml-3" v-for="spells in classes">
       <v-card-title>{{ spells.name }}</v-card-title>
       <p>{{ spells.desc }}</p>
-    </v-card> -->
-    
-  </div> 
+    </v-card>-->
+    <v-row>
+      <v-col cols="3">
+        <v-card v-for="spells in classes" class="">
+          <v-card-title>{{ spells.name }}</v-card-title>
+          <div class="d-flex flex-column justify-start align-start">
+            <v-card-subtitle>{{ spells.level }}</v-card-subtitle>
+            <v-card-subtitle>{{ spells.school }}</v-card-subtitle>
+          </div>
+          <v-card-subtitle> {{ spells.dnd_class }} </v-card-subtitle>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
@@ -14,35 +25,27 @@ import axios from "axios";
 export default {
   name: "spells",
   methods: {
-      getSpells: (myUrl) => {
-        // console.log(myUrl)
-    },
-  },//end methods
+    getSpells: myUrl => {
+      // console.log(myUrl)
+    }
+  }, //end methods
   data() {
     return {
       classes: null
     };
-  },//end data
+  }, //end data
   mounted() {
     const axios = require("axios");
-    const url =
-      "https://api.open5e.com/spells/?format=json";
+    const url = "https://api.open5e.com/spells/?format=json";
 
     axios
       .get(url)
-        // .then(response => console.log(response.data.results))
+      // .then(response => console.log(response.data.results))
       .then(res => {
         this.classes = res.data.results;
       })
       .catch(error => console.log(error));
-
-
-
-
-
-
-
-  },//end mounted
+  } //end mounted
 };
 </script>
 
