27b65d0f24af7a7a5f8f502ba9ebfde6b05ba442
manoy96 <mfewcar@gmail.com>
2019-10-16 00:49:41 -0600

    getting some issues with dividers

--- src/components/spells.vue
+++ src/components/spells.vue
@@ -3,46 +3,47 @@
     <v-row>
       <v-col cols="3" v-for="spells in classes" :key="spells.document__slug" class="mySpell">
         <h1 class="title">{{ spells.name }}</h1>
-        <h2 class="body-1">{{ spells.level }} {{ spells.school }}</h2>
+        <h2 class="body-1">{{ spells.level }} - {{ spells.school }}</h2>
         <h2 class="body-1">{{ spells.dnd_class }}</h2>
         <v-divider></v-divider>
         <div class="myCast">
-          <h3 class="body-1 conc">Concentration: {{ spells.concentration }}</h3>
+          <h3 class="body-1">Concentration: {{ spells.concentration }}</h3>
           <v-divider vertical></v-divider>
-          <h3 class="body-1 conc">Ritual: {{ spells.ritual }}</h3>
+          <h3 class="body-1">Ritual: {{ spells.ritual }}</h3>
         </div>
         <v-divider></v-divider>
-        <div class="spellInfo">
-          <div class="castTime">
-            <v-chip class="myChip" color="success" outlined>
+        <div class="spellGroup">
+          <div class="spellItem castTime">
+            <v-chip class="myChip" color="primary" outlined>
               <i class="far fa-clock fa-1x"></i>
               <p class="body-1 castHead">Casting Time</p>
             </v-chip>
             <h3 class="body-1">{{ spells.casting_time }}</h3>
           </div>
-          <div class="components">
-            <v-chip class="myChip" color="success" outlined>
-              <i class="far fa-clock fa-1x"></i>
-              <p class="body-1 castHead">Casting Time</p>
+          <div class="spellItem castTime">
+            <v-chip class="myChip" color="primary" outlined>
+              <i class="fas fa-scroll fa-1x"></i>
+              <p class="body-1 castHead">Components</p>
             </v-chip>
-            <h3 class="body-1">{{ spells.casting_time }}</h3>
+            <h3 class="body-1">{{ spells.components }}</h3>
           </div>
-          <div class="duration">
-            <v-chip class="myChip" color="success" outlined>
-              <i class="far fa-clock fa-1x"></i>
-              <p class="body-1 castHead">Casting Time</p>
+          <div class="spellItem castTime">
+            <v-chip class="myChip" color="primary" outlined>
+              <i class="fas fa-hourglass-half fa-1x"></i>
+              <p class="body-1 castHead">Duration</p>
             </v-chip>
-            <h3 class="body-1">{{ spells.casting_time }}</h3>
+            <h3 class="body-1">{{ spells.duration }}</h3>
           </div>
-          <div class="range">
-            <v-chip class="myChip" color="success" outlined>
-              <i class="far fa-clock fa-1x"></i>
-              <p class="body-1 castHead">Casting Time</p>
+          <div class="spellItem castTime">
+            <v-chip class="myChip" color="primary" outlined>
+              <i class="fas fa-bullseye fa-1x"></i>
+              <p class="body-1 castHead">Range</p>
             </v-chip>
-            <h3 class="body-1">{{ spells.casting_time }}</h3>
+            <h3 class="body-1">{{ spells.range }}</h3>
           </div>
         </div>
-
+        <v-divider></v-divider>
+        <p class="body-2"> {{ spells.desc }} </p>
       </v-col>
     </v-row>
   </div>
@@ -75,6 +76,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.myDivide {
+  margin: 1rem 0;
+}
+.vertDivide {
+  margin: 1rem;
+}
 .mySpell {
   margin: 1rem;
   padding: 0.5rem;
@@ -83,40 +90,34 @@ export default {
     display: flex;
     flex-direction: row;
     justify-content: flex-start;
-    flex-wrap: wrap;
+    flex-wrap: wrap
 
-    .conc {
-      margin: 0.5rem 0.25rem;
-    }
   }
-  .spellInfo {
+  .spellGroup {
+    padding: 1rem 0;
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
+    justify-content: center;
 
-    .castTime, .components, .duration, .range {
+    .spellItem {
+      width: 40%;
+      margin: 0 .75rem;
+
       display: flex;
       flex-direction: column;
       align-items: center;
+      .myChip {
+        width: 100%;
 
-      width: 49%;
-    }
-    .castTime {
-      
-      .castHead {
-        margin: 0.25rem 0.5rem;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        margin: .5rem;
+        .castHead {margin: 0 .5rem;}
       }
     }
-    .components {
-    }
-    .duration {
-    }
-    .range {
-    }
   }
-}
-.myChip {
-  width: 100%;
 }
 </style>
 
