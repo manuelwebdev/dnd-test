a27131b2bd9298156f2238e4cbe71c20a66ddc04
manoy96 <mfewcar@gmail.com>
2019-11-02 23:19:05 -0600

    router isn't working. need to fix it

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import SpellBook from "./views/SpellBook.vue";
 
 Vue.use(Router);
 
@@ -10,12 +11,11 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "home",
       component: Home
     },
     {
       path: "/spellbook",
-      name: "spellbook",
+      component: SpellBook
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
