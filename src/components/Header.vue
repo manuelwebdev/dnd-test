e76198bf79f993b598ff1a649b2b556da0099986
manoy96 <mfewcar@gmail.com>
2019-12-14 17:54:58 -0700

    bringing in a side menu drawer

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,11 @@
 <template>
-  <v-card color="grey lighten-4" flat height="200px" tile>
-    <v-toolbar dense>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  <v-card class="mx-auto overflow-hidden" height="400" width="344">
+    <v-system-bar color="deep-purple darken-3"></v-system-bar>
 
-      <v-toolbar-title>D & D</v-toolbar-title>
+    <v-app-bar color="deep-purple accent-4" dark prominent>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>My files</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
@@ -12,18 +14,53 @@
       </v-btn>
 
       <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
+        <v-icon>mdi-filter</v-icon>
       </v-btn>
 
       <v-btn icon>
         <v-icon>mdi-dots-vertical</v-icon>
       </v-btn>
-    </v-toolbar>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute bottom temporary>
+      <v-list nav dense>
+        <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
+          <v-list-item>
+            <v-list-item-title>Foo</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Bar</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Fizz</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Buzz</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-card-text>The navigation drawer will appear from the bottom on smaller size screens.</v-card-text>
   </v-card>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    drawer: false,
+    group: null
+  }),
+
+  watch: {
+    group() {
+      this.drawer = false;
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
